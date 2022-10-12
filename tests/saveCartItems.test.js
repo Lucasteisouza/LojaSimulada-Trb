d75c67339268a1e919d014c0d13c7983b4a7f491
Lucasteisouza <lucasteisouza@gmail.com>
2022-10-11 21:52:37 -0300

    Requisio 6 atendido

--- tests/saveCartItems.test.js
+++ tests/saveCartItems.test.js
@@ -2,8 +2,14 @@ const localStorageSimulator = require('../mocks/localStorageSimulator');
 const saveCartItems = require('../helpers/saveCartItems');
 
 localStorageSimulator('setItem');
-
+const cartItem = 'ID: MLB1937076326 | TITLE: Pc Computador Cpu Core I5 650 + Ssd 240gb, 8gb Memória Ram | PRICE: $1050';
 describe('3 - Teste a função saveCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('should use localStorage.setItem when called with a cartItem argument', () => {
+    saveCartItems(cartItem);
+    expect(localStorage.setItem).toHaveBeenCalled();
+  });
+  it('should call localStorage.setItems(cartItems, the value used as argument for saveCartItems)', () => {
+    saveCartItems(cartItem);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cartItem', cartItem)
+  })
 });
